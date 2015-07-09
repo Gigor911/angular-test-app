d97d3807870d413d7bd5f3f527b9c27bb6000ba1
Ihor <gigor911@gmail.com>
2015-07-09 19:19:37 +0300

    gitignore

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var speroteck = angular.module('speroteck-site', ['ngRoute']);
-speroteck.config(['$routeProvider',function($routeProvider) {
-	$routeProvider.
-		when('/', {
-			templateUrl: 'app/views/home.html'
-		});
+speroteck.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: 'app/views/home.html'
+        });
 }]);
--- app/directives/directives.js
+++ app/directives/directives.js
@@ -1,14 +1,14 @@
-speroteck.directive('siteHeader', function(){
-	return {
-		// controller: function($scope, $element, $attrs, $transclude) {},
-		restrict: 'E',
-		templateUrl: 'app/views/partials/header.html',
-	};
+speroteck.directive('siteHeader', function () {
+    return {
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        restrict: 'E',
+        templateUrl: 'app/views/partials/header.html',
+    };
 });
-speroteck.directive('siteFooter', function(){
-	return {
-		// controller: function($scope, $element, $attrs, $transclude) {},
-		restrict: 'E',
-		templateUrl: 'app/views/partials/footer.html',
-	};
+speroteck.directive('siteFooter', function () {
+    return {
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        restrict: 'E',
+        templateUrl: 'app/views/partials/footer.html',
+    };
 });
